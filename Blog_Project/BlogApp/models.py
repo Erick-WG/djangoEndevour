c55000bd0da5bf24ba9fae194b721f9cbaa4f59a
Erick-WG <erix.academix@gmail.com>
2023-04-09 20:45:36 +0300

    adding tag identities to blog posts
    Adding this field in our database helps in future referencing for the little title in the web browsers.

--- Blog_Project/BlogApp/models.py
+++ Blog_Project/BlogApp/models.py
@@ -1,9 +1,11 @@
+from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
 
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    title_tag = models.CharField(max_length=255, default = "This is my Awesome Blog post.")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
 
